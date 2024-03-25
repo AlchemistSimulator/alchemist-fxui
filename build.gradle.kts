4cfcb4c31504c9028159c399c78a9018790fbb85
Danilo Pianini <danilo.pianini@gmail.com>
2024-03-25 21:17:02 +0100

    chore: initialize fxui from the main repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,36 @@
-@Suppress("DSL_SCOPE_VIOLATION")
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
 plugins {
     alias(libs.plugins.dokka)
     alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.java.qa)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.qa)
-    alias(libs.plugins.publishOnCentral)
     alias(libs.plugins.multiJvmTesting)
+    alias(libs.plugins.publishOnCentral)
     alias(libs.plugins.taskTree)
 }
 
-group = "org.danilopianini"
+group = "it.unibo.alchemist"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        content {
+            includeGroup("no.tornado")
+        }
+    }
 }
 
-dependencies {
-    implementation(libs.kotlin.stdlib)
-    testImplementation(libs.bundles.kotlin.testing)
+multiJvm {
+    jvmVersionForCompilation.set(17)
 }
 
 kotlin {
@@ -25,10 +38,42 @@ kotlin {
         compilations.all {
             kotlinOptions {
                 allWarningsAsErrors = true
-                freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
             }
         }
     }
+}
+
+dependencies {
+    compileOnly(libs.spotbugs.annotations)
+    api(libs.bundles.alchemist)
+    implementation(libs.bundles.jiconfont)
+    implementation(libs.apache.commons.collections4)
+    implementation(libs.controlsfx)
+    implementation(libs.gson.extras)
+    implementation(libs.guava)
+    implementation(libs.javafxSvg)
+    implementation(libs.jfoenix)
+    implementation(libs.leafletmap)
+    implementation(libs.resourceloader)
+    implementation(libs.slf4j)
+    implementation(libs.tornadofx)
+    val javaFXVersion = "11"
+    for (platform in listOf("linux", "mac", "win")) {
+        api("org.openjfx:javafx-base:$javaFXVersion:$platform")
+        api("org.openjfx:javafx-controls:$javaFXVersion:$platform")
+        api("org.openjfx:javafx-fxml:$javaFXVersion:$platform")
+        api("org.openjfx:javafx-graphics:$javaFXVersion:$platform")
+        api("org.openjfx:javafx-media:$javaFXVersion:$platform")
+        api("org.openjfx:javafx-swing:$javaFXVersion:$platform")
+        api("org.openjfx:javafx-web:$javaFXVersion:$platform")
+    }
+    testImplementation(libs.junit.api)
+    testRuntimeOnly(libs.junit.engine)
+    testRuntimeOnly(libs.alchemist.incarnation.protelis)
+}
+
+tasks.javadoc {
+    enabled = false
 }
 
 tasks.test {
@@ -49,23 +94,25 @@ signing {
 }
 
 publishOnCentral {
-    projectLongName.set("Template Kotlin JVM Project")
-    projectDescription.set("A template repository for Kotlin JVM projects")
-    repository("https://maven.pkg.github.com/danysk/${rootProject.name}".lowercase()) {
-        user.set("DanySK")
-        password.set(System.getenv("GITHUB_TOKEN"))
-    }
-    publishing {
-        publications {
-            withType<MavenPublication> {
-                pom {
-                    developers {
-                        developer {
-                            name.set("Danilo Pianini")
-                            email.set("danilo.pianini@gmail.com")
-                            url.set("http://www.danilopianini.org/")
-                        }
-                    }
+    projectLongName.set(project.properties["projectLongName"] as String)
+    projectDescription.set(project.properties["projectDescription"] as String)
+}
+
+publishing.publications {
+    withType<MavenPublication> {
+        pom {
+            developers {
+                developer {
+                    name.set("Niccolò Maltoni")
+                    email.set("niccolo.maltoni@studio.unibo.it")
+                }
+                developer {
+                    name.set("Vuksa Mihajlovic")
+                    email.set("vuksa.mihajlovic@studio.unibo.it")
+                }
+                developer {
+                    name.set("Danilo Pianini")
+                    email.set("danilo.pianini@unibo.it")
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,18 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
 plugins {
     id("com.gradle.enterprise") version "3.16.2"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.4"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
+rootProject.name = "alchemist-fxui"
 
 gradleEnterprise {
     buildScan {
@@ -13,11 +23,9 @@ gradleEnterprise {
 }
 
 gitHooks {
-    preCommit {
-        tasks("ktlintCheck")
-    }
     commitMsg { conventionalCommits() }
-    createHooks()
+    preCommit {
+        tasks("ktlintCheck", "--parallel")
+    }
+    createHooks(overwriteExisting = true)
 }
-
-rootProject.name = "Template-for-Kotlin-JVM-Projects"
